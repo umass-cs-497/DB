16147aaac2be68fe495765fa463ee77aa1b6cce0
Santiago <sdelatorrepi@umass.edu>
2015-03-12 23:11:25 -0400

    Added Users schema

--- users.js
+++ users.js
@@ -2,9 +2,31 @@ var mongoose = require('mongoose');//using mongoose to connect
 //connects to local machine.
 mongoose.connect('mongodb://localhost/lecture');
 
-//database connection
+//database connectionn 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(callback){
 	console.log("connected");
+});
+
+var userSchema = new mongoose.Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: String,
+
+  firstName: String,
+  lastName: String,
+  role: String,	
+  profilePic: String,
+
+  course: {
+    title: { type: String },
+    department: { type: String },
+    section: { type: String },
+    term: { type: String },
+     year: {type: String}
+  },
+
+
+
+
 });
