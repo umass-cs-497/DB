5bd3c1c6d7489f47cf0ad1d0b3ad5f128d447a51
Santiago <sdelatorrepi@umass.edu>
2015-03-12 23:18:04 -0400

    Added course schema to the course db

--- courses.js
+++ courses.js
@@ -8,3 +8,20 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(callback){
 	console.log("connected");
 });
+
+//course schema
+var courseSchema = new mongoose.Schema({
+  ObjectId: String,
+  title: String,
+  department: String,
+  courseNumber: String,	
+  description: String,
+  courseSection: String,
+  semester: String,
+  instructor: { type: String, unique: true},
+  roaster: {type: String, unique: true},
+  lecture: String	
+
+
+
+});
