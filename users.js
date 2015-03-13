0d9c9c845395c9b16aabf91db155b6a7b3cd4e05
Santiago <sdelatorrepi@umass.edu>
2015-03-12 23:10:22 -0400

    Added connection

--- users.js
+++ users.js
@@ -1,3 +1,10 @@
 var mongoose = require('mongoose');//using mongoose to connect
 //connects to local machine.
 mongoose.connect('mongodb://localhost/lecture');
+
+//database connection
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback){
+	console.log("connected");
+});
