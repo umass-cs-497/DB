1998eb22d70cf0c23eb4e0bb789b8a126862cfbe
Santiago <sdelatorrepi@umass.edu>
2015-03-27 16:09:03 -0400

    preconditions not working

--- test/testCourses.js
+++ test/testCourses.js
@@ -9,15 +9,41 @@ describe('Testing Courses collection:', function(){
 	 var testCourse = null;
 	 before(function(done){
 	 	db_api.courses.dropCoursesDatabase(function(){
-	 		db_api.courses.createCourse('Fall','Sociology','SOC101',function(err,course){
+	 		db_api.courses.createCourse('Fall','Sociology','SOC101', function(err,course) {
 	 		  testCourse = course;
 	 		  assert.equal(err,null);
 	 		  assert.norEqual(testCourse, null);
 	 		  assert.equal(testCourse.semester, 'Fall');
 	 		  assert.equal(testCourse.department,'Sociology');
-	 		  assert.equal(testCourse.courseNumber, 'SOC10');
+	 		  assert.equal(testCourse.courseNumber, 'SOC101');
 	 		});
 	 	});
 	 });
 
+	 /*
+	  * Post-condition
+	  */
+	  // after(function(done){
+	  // 	db_api.courses.dropCoursesDatabase(function(err, message){
+	  // 		console.log(message);
+	  // 		assert.equal(err,null);
+	  // 		assert.notEqual(message,0);
+	  // 		message.should.eql(1);
+	  // 		done();
+	  // 	});
+	  // });
+
+	  /*
+	   * 
+	   */
+	   it('creates a new course based on semester, department and courseNumber: Fall, Sociology, SOC101',function(done){
+		    db_api.courses.createCourse('Fall','Sociology','SOC101',function(err,course){
+	 		  assert.equal(err,null);
+	 		  assert.norEqual(courses, null);
+	 		  assert.equal(courses.semester, 'Fall');
+	 		  assert.equal(course.department,'Sociology');
+	 		  assert.equal(course.courseNumber, 'SOC101');
+	 		  done();
+		 	});
+		}); 	
 });
