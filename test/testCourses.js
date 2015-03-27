efeb9ff4ea87a373005d54b3b4a1a3fd6baf40b9
Santiago <sdelatorrepi@umass.edu>
2015-03-27 14:30:58 -0400

    preconditions for testCourses.js

--- test/testCourses.js
+++ test/testCourses.js
@@ -6,9 +6,17 @@ describe('Testing Courses collection:', function(){
 	/*
 	 * Precondition
 	 */
+	 var testCourse = null;
 	 before(function(done){
 	 	db_api.courses.dropCoursesDatabase(function(){
-
+	 		db_api.courses.createCourse('Fall','Sociology','SOC101',function(err,course){
+	 		  testCourse = course;
+	 		  assert.equal(err,null);
+	 		  assert.norEqual(testCourse, null);
+	 		  assert.equal(testCourse.semester, 'Fall');
+	 		  assert.equal(testCourse.department,'Sociology');
+	 		  assert.equal(testCourse.courseNumber, 'SOC101');
+	 		});
 	 	});
 	 });
 
